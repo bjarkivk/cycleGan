7a9044cd83946276ced05528aa473211ff98b60f
Jón Hlöðver Friðriksson <40834746+NonniHlolli@users.noreply.github.com>
2021-05-03 14:37:06 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,15 +12,28 @@ def load_images(images):
 
 # returns dataA(apples) and dataB(oranges)
 def load_dataset(dataset):
-    dir = 'cycle_gan/'+dataset
+    dir = 'cycle_gan/'+ dataset
     ds = tfds.load(dir, with_info=False)
     ds = tfds.as_numpy(ds)
 
     trainA = load_images(ds['trainA'])
     testA = load_images(ds['testA'])
     dataA = vstack((trainA, testA))
+    dataA = (dataA - 127.5) / 127.5
 
     trainB = load_images(ds['trainB'])
     testB = load_images(ds['testB'])
     dataB = vstack((trainB, testB))
+    dataB = (dataB - 127.5) / 127.5
     return dataA, dataB
+
+
+# select a batch of random samples, returns images and target
+def generate_real_samples(data, n_samples):
+	# choose random instances
+	ix = randint(0, data.shape[0], n_samples)
+	# retrieve selected images
+	X = data[ix]
+	# generate 'real' class labels (1)
+	y = ones(n_samples)
+	return X, y
