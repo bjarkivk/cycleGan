f92d10368d051dd432563c5e05ef3781baf1b896
Bjarki Viðar Kristjánsson <bvkr@kth.se>
2021-05-07 14:20:08 +0200

    add train and training_options files

---
+++ training_options.py
@@ -0,0 +1,6 @@
+class TrainOptions():
+
+    def __init__(self, n_epochs=10, epoch_count=0, n_epochs_decays=0):
+        self.n_epochs = n_epochs
+        self.epoch_count = epoch_count
+        self.n_epochs_decays = n_epochs_decays
