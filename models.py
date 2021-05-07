21cdf3a3ab0ecda3b336ea5b9abd1428cfe2a311
Jón Hlöðver Friðriksson <40834746+NonniHlolli@users.noreply.github.com>
2021-05-07 14:00:04 +0000

    Update models.py

--- models.py
+++ models.py
@@ -27,13 +27,20 @@ def get_scheduler(optimizer, opt):
 
 def init_weights(net, init_type='normal', init_gain=0.02):
     """Initialize network weights. Possibilities are 'Normal' and 'Xavier'"""
-    def init_func(m):  # define the initialization function
-        if init_type == 'normal':
-            init.normal_(m.weight.data, 0.0, init_gain)
-        elif init_type == 'xavier':
-            init.xavier_normal_(m.weight.data, gain=init_gain)
-        if hasattr(m, 'bias') and m.bias is not None:
+        def init_func(m):  # define the initialization function
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
                 init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            init.normal_(m.weight.data, 1.0, init_gain)
+            init.constant_(m.bias.data, 0.0)
 
     print('initialize network with %s' % init_type)
     net.apply(init_func)  # apply the initialization function <init_func>
