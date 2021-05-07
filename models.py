bc024552fa725cfc2a53c34b35d83d27e5e3abc5
Jón Hlöðver Friðriksson <40834746+NonniHlolli@users.noreply.github.com>
2021-05-07 15:22:45 +0000

    Update models.py

--- models.py
+++ models.py
@@ -4,12 +4,6 @@ from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
 
-
-class Identity(nn.Module):
-    def forward(self, x):
-        return x
-
-
 def get_norm_layer():
     """Returns an instance normalization layer"""
     norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
@@ -25,28 +19,20 @@ def get_scheduler(optimizer, opt):
     return scheduler
 
 
-def init_weights(net, init_type='normal', init_gain=0.02):
+def init_weights(net, init_gain=0.02):
     """Initialize network weights. Possibilities are 'Normal' and 'Xavier'"""
     def init_func(m):  # define the initialization function
         classname = m.__class__.__name__
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
-            if init_type == 'normal':
-                init.normal_(m.weight.data, 0.0, init_gain)
-            elif init_type == 'xavier':
-                init.xavier_normal_(m.weight.data, gain=init_gain)
-            else:
-                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            init.normal_(m.weight.data, 0.0, init_gain)
             if hasattr(m, 'bias') and m.bias is not None:
                 init.constant_(m.bias.data, 0.0)
-        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
-            init.normal_(m.weight.data, 1.0, init_gain)
-            init.constant_(m.bias.data, 0.0)
 
     print('initialize network with %s' % init_type)
     net.apply(init_func)  # apply the initialization function <init_func>
 
 
-def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
+def init_net(net, init_gain=0.02, gpu_ids=[]):
     """Initialize a network: 1. register CPU/GPU device (with multi-GPU support); 2. initialize the network weights
     Parameters:
         net (network)      -- the network to be initialized
@@ -59,46 +45,39 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
         assert(torch.cuda.is_available())
         net.to(gpu_ids[0])
         net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs
-    init_weights(net, init_type, init_gain=init_gain)
+    init_weights(net, init_gain=init_gain)
     return net
 
 
-def Generator(input_nc, output_nc, ngf,  use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[]):
+def Generator(input_nc, output_nc, ngf,  use_dropout=False, init_gain=0.02, gpu_ids=[]):
     """Create a generator
     Parameters:
         input_nc (int) -- the number of channels in input images
         output_nc (int) -- the number of channels in output images
         ngf (int) -- the number of filters in the last conv layer
-        netG (str) -- the architecture's name: resnet_9blocks | resnet_6blocks | unet_256 | unet_128
-        norm (str) -- the name of normalization layers used in the network: batch | instance | none
         use_dropout (bool) -- if use dropout layers.
-        init_type (str)    -- the name of our initialization method.
         init_gain (float)  -- scaling factor for normal, xavier and orthogonal.
         gpu_ids (int list) -- which GPUs the network runs on: e.g., 0,1,2
     Returns a 9 Resnet block generator
     """
     norm_layer = get_norm_layer()
-    net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
-    return init_net(net, init_type, init_gain, gpu_ids)
+    net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
+    return init_net(net, init_gain, gpu_ids)
 
 
-def Discriminator(input_nc, ndf, netD, n_layers_D=3, init_type='normal', init_gain=0.02, gpu_ids=[]):
+def Discriminator(input_nc, ndf, n_layers_D=3,  init_gain=0.02, gpu_ids=[]):
     """Create a discriminator
     Parameters:
         input_nc (int)     -- the number of channels in input images
         ndf (int)          -- the number of filters in the first conv layer
-        netD (str)         -- the architecture's name: basic | n_layers | pixel
         n_layers_D (int)   -- the number of conv layers in the discriminator; effective when netD=='n_layers'
-        norm (str)         -- the type of normalization layers used in the network.
-        init_type (str)    -- the name of the initialization method.
         init_gain (float)  -- scaling factor for normal, xavier and orthogonal.
         gpu_ids (int list) -- which GPUs the network runs on: e.g., 0,1,2
     Returns a 'PatchGAN' discriminator
     """
-   
     norm_layer = get_norm_layer()
     net = NLayerDiscriminator(input_nc, ndf, n_layers=3, norm_layer=norm_layer)
-    return init_net(net, init_type, init_gain, gpu_ids)
+    return init_net(net, init_gain, gpu_ids)
 
 
 ##############################################################################
@@ -113,7 +92,7 @@ class GANLoss(nn.Module):
     def __init__(self, gan_mode, target_real_label=1.0, target_fake_label=0.0):
         """ Initialize the GANLoss class.
         Parameters:
-            gan_mode (str) - - the type of GAN objective. It currently supports vanilla, lsgan.
+            gan_mode (str) - - the type of GAN objective. It currently supports lsgan.
             target_real_label (bool) - - label for a real image
             target_fake_label (bool) - - label of a fake image
         LSGAN needs no sigmoid. vanilla GANs will handle it with BCEWithLogitsLoss.
@@ -122,10 +101,7 @@ class GANLoss(nn.Module):
         self.register_buffer('real_label', torch.tensor(target_real_label))
         self.register_buffer('fake_label', torch.tensor(target_fake_label))
         self.gan_mode = gan_mode
-        if gan_mode == 'lsgan':
-            self.loss = nn.MSELoss()
-        elif gan_mode == 'vanilla':
-            self.loss = nn.BCEWithLogitsLoss()
+        self.loss = nn.MSELoss()
 
     def get_target_tensor(self, prediction, target_is_real):
         """Create label tensors with the same size as the input.
@@ -135,7 +111,6 @@ class GANLoss(nn.Module):
         Returns:
             A label tensor filled with ground truth label, and with the size of the input
         """
-
         if target_is_real:
             target_tensor = self.real_label
         else:
@@ -149,7 +124,6 @@ class GANLoss(nn.Module):
             target_is_real (bool) - - if the ground truth label is for real images or fake images
         Returns:
             the calculated loss."""
-
         target_tensor = self.get_target_tensor(prediction, target_is_real)
         loss = self.loss(prediction, target_tensor)
         return loss
@@ -159,7 +133,7 @@ class ResnetGenerator(nn.Module):
     """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
     """
 
-    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=9, padding_type='zero'):
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, padding_type='zero'):
         """Construct a Resnet-based generator
         Parameters:
             input_nc (int)      -- the number of channels in input images
@@ -167,7 +141,6 @@ class ResnetGenerator(nn.Module):
             ngf (int)           -- the number of filters in the last conv layer
             norm_layer          -- normalization layer
             use_dropout (bool)  -- if use dropout layers
-            n_blocks (int)      -- the number of ResNet blocks
             padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
         """
         super(ResnetGenerator, self).__init__()
@@ -304,5 +277,4 @@ class NLayerDiscriminator(nn.Module):
     def forward(self, input):
         """Standard forward."""
         return self.model(input)
-
 
