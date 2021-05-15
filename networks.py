9af48735032771d1a3b95bf53777251d5717cc0e
Teresa Ho <teresa1995885@gmail.com>
2021-05-15 19:49:26 +0200

    training wip

--- networks.py
+++ networks.py
@@ -108,11 +108,11 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
 
     Return an initialized network.
     """
-    # if len(gpu_ids) > 0:
-    #     assert(torch.cuda.is_available())
-    #     net.to(gpu_ids[0])
-    #     net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs
-    #net.to(gpu_ids[0])
+    if len(gpu_ids) > 0:
+        assert(torch.cuda.is_available())
+        net.to(gpu_ids[0])
+        net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs
+    net.to(gpu_ids[0])
     init_weights(net, init_type, init_gain=init_gain)
     return net
 
