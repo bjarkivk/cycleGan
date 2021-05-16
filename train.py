b221901645545ef5607eeaa4cd8b40fa40f8b372
Teresa Ho <teresa1995885@gmail.com>
2021-05-16 23:47:11 +0200

    train.py modified

--- train.py
+++ train.py
@@ -1,24 +1,8 @@
-"""General-purpose training script for image-to-image translation.
-This script works for various models (with option '--model': e.g., pix2pix, cyclegan, colorization) and
-different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
-You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
-It first creates model, dataset, and visualizer given the option.
-It then does standard network training. During the training, it also visualize/save the images, print/save the loss plot, and save models.
-The script supports continue/resume training. Use '--continue_train' to resume your previous training.
-Example:
-    Train a CycleGAN model:
-        python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
-    Train a pix2pix model:
-        python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
-See options/base_options.py and options/train_options.py for more training options.
-See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
-See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
-"""
 import time
 from options.train_options import TrainOptions
 from data import create_dataset
 #from models import create_model
-from util.visualizer import Visualizer
+#from util.visualizer import Visualizer
 from cycleGan import *
 
 
@@ -33,6 +17,7 @@ if __name__ == '__main__':
     # model.setup(opt)               # regular setup: load and print networks; create schedulers
     model = CycleGANModel(opt)
     model.setup(opt)
+    #visualizer = Visualizer(opt)
 
     total_iters = 0                # the total number of training iterations
 
@@ -40,12 +25,14 @@ if __name__ == '__main__':
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
-        print("current epoch : "+str(epoch))
+        #visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
+        print("current epoch:  "+str(epoch))
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
-
-            print("total_iters:  "+str(total_iters))
+            if total_iters % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
+            print("current iter:  "+str(total_iters))
             total_iters += opt.batch_size
             epoch_iter += opt.batch_size
             model.set_input(data)         # unpack data from dataset and apply preprocessing
@@ -54,15 +41,13 @@ if __name__ == '__main__':
             if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+                #visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
                 t_comp = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
-                if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
-
+                #visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+                
 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
                 print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
